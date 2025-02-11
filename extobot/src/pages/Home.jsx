81744182b2ce916e8aed06283a96a60ca3f20fc5
SK Ahidulla <79365693+Nabab-Hub@users.noreply.github.com>
2025-02-11 23:49:51 +0530

    Update Home.jsx

--- extobot/src/pages/Home.jsx
+++ extobot/src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import './Home.css'
-
-
+import { NavLink } from 'react-router-dom'
 
 export default function Home() {
+
     return (
         <div className="home-page">
-            This is Home Page 
+            <h2>This is Home Page </h2>
+
+            <h3 style={{marginTop:"5rem"}}>Interviews</h3>
+            <ul style={{marginLeft: "5rem"}}>
+              <li><NavLink to="/interviews/slot1">Slot1</NavLink></li>
+              <li><NavLink to="/interviews/slot2">Slot2</NavLink></li>
+              <li><NavLink to="/interviews/slot3">Slot3</NavLink></li>
+              <li><NavLink to="/interviews/slot4">Slot4</NavLink></li>
+              <li><NavLink to="/interviews/slot5">Slot5</NavLink></li>
+            </ul>
         </div>
     )
 }
