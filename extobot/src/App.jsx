ce427644a476d0c8c21eb8c136953d90a16d499f
SK Ahidulla <79365693+Nabab-Hub@users.noreply.github.com>
2025-03-29 20:57:55 +0530

    Update App.jsx

--- extobot/src/App.jsx
+++ extobot/src/App.jsx
@@ -1,31 +1,24 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import Home from './pages/Home';
-import InterviewQuestions1 from './pages/interviews/slot1/InterviewQuestions';
-import InterviewQuestions2 from './pages/interviews/slot2/InterviewQuestions';
-import InterviewQuestions3 from './pages/interviews/slot3/InterviewQuestions';
-import InterviewQuestions4 from './pages/interviews/slot4/InterviewQuestions';
-import InterviewQuestions5 from './pages/interviews/slot5/InterviewQuestions';
-import ErrorPage from './pages/ErrorPage'
+import ErrorPage from './pages/ErrorPage';
+import ExamCrackers from './pages/ExamCrackers';
 
 
 function App() {
 
   return (
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/interviews/slot1' element={<InterviewQuestions1 />} />
-            <Route path='/interviews/slot2' element={<InterviewQuestions2 />} />
-            <Route path='/interviews/slot3' element={<InterviewQuestions3 />} />
-            <Route path='/interviews/slot4' element={<InterviewQuestions4 />} />
-            <Route path='/interviews/slot5' element={<InterviewQuestions5 />} />
-            <Route path='/*' element={<ErrorPage />} />
-          </Routes>
-        </BrowserRouter>
-      </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/exam-cracker/:slotid' element={<ExamCrackers />} />
+          <Route path='/*' element={<ErrorPage />} />
+
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
